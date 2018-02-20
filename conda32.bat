77bc10cf107bc97df9a5ac9ca2664cb2ddc44e5d
gyusu han <slhopy@gmail.com>
2018-02-20 23:59:02 +0900

    improve for multiple hard drive environment

--- conda32.bat
+++ conda32.bat
@@ -1,1 +1,1 @@
-@start Anaconda_Prompt_32bit(admin) /k "%ANACONDA_32BIT%\Scripts\activate.bat %ANACONDA_32BIT% & cd %cd%"
+@start Anaconda_Prompt_32bit(admin) /k "%ANACONDA_32BIT%\Scripts\activate.bat %ANACONDA_32BIT% & cd /d %cd%"
