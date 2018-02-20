5faae6629b9b924f1536f0955b584493d30f09e0
gyusu han <slhopy@gmail.com>
2018-02-20 23:59:32 +0900

    improve for multiple hard drive environment

--- conda64.bat
+++ conda64.bat
@@ -1,1 +1,1 @@
-@start Anaconda_Prompt_64bit(admin) /k "%ANACONDA_64BIT%\Scripts\activate.bat %ANACONDA_64BIT% & cd %cd%"
+@start Anaconda_Prompt_64bit(admin) /k "%ANACONDA_64BIT%\Scripts\activate.bat %ANACONDA_64BIT% & cd /d %cd%"
